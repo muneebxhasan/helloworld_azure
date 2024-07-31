43be0d07b5a2d7fa5cc78493bd0959254c6d3d4e
Muneeb Hassan <muneebxhasan@gmail.com>
2024-07-31 07:10:16 +0500

    c1

--- quizapp/main.py
+++ quizapp/main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-app.get('/about')
+@app.get('/about')
 def about():
     return {"about": "This is a hello world from azure containerr"}    
